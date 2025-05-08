12d3b22a04466e7b6b3a1fda5c7ef471f8af3436
reus <reusee@gmail.com>
2025-05-08 09:25:44 +0800

    update

--- prepare.sh
+++ prepare.sh
@@ -1,11 +1,14 @@
+#!/bin/bash
+
 set -x -e
 
 # install debootstrap
 sudo dnf install -y debootstrap
 
-# prepare root filesystem
+# prepare template root filesystem
 ROOTDIR=./root
 CACHEDIR=$(realpath ./pkg-cache)
 mkdir -p $CACHEDIR
 sudo rm -rf $ROOTDIR
 sudo debootstrap --cache-dir=$CACHEDIR noble $ROOTDIR https://mirrors.cloud.tencent.com/ubuntu/ 
+
--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -x -e
+
 sudo systemd-nspawn \
   --template=./root \
   --directory=root-$(date -Isec) \
