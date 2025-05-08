ffe65f73d1ccdd74db22e94a309e615ed3070b1c
reus <reusee@gmail.com>
2025-05-08 09:58:19 +0800

    update

--- prepare.sh
+++ prepare.sh
@@ -12,3 +12,6 @@ mkdir -p $CACHEDIR
 sudo rm -rf $ROOTDIR
 sudo debootstrap --cache-dir=$CACHEDIR noble $ROOTDIR https://mirrors.cloud.tencent.com/ubuntu/ 
 
+# set root password
+echo "set root password"
+sudo systemd-nspawn -D ./root passwd
