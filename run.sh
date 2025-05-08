b00fb93e82812d76c6528916ab9ee814ca79fee6
reus <reusee@gmail.com>
2025-05-08 10:10:46 +0800

    update

--- run.sh
+++ run.sh
@@ -2,9 +2,14 @@
 
 set -x -e
 
+if [ -z "$1" ]; then
+  DIR="--template=./root --directory=root-$(date -Isec)"
+else
+  DIR="--directory=$1"
+fi
+
 sudo systemd-nspawn \
-  --template=./root \
-  --directory=root-$(date -Isec) \
+  $DIR \
   --boot \
   --private-network \
   --network-veth \
